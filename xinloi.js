d0a790f6ffaa49ab893d51779407d644e4f775ab
duyalan04 <nd83455@gmail.com>
2024-07-24 09:10:46 +0700

    change

--- xinloi.js
+++ xinloi.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Anh iu bé ,Anh sẽ hongg làm bé buồn nữa đouuu ";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
